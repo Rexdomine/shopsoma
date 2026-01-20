ac9adf359c3e5c4c77c132c58411ea9d7927713c
rexdomine <dominusparte@gmail.com>
2026-01-20 17:39:59 +0100

    fix: close vendor product add layout wrapper

--- shopsoma-frontend/src/pages/vendor/VendorProductAdd.tsx
+++ shopsoma-frontend/src/pages/vendor/VendorProductAdd.tsx
@@ -1698,6 +1698,7 @@ export default function VendorProductAdd() {
         onClose={() => setShowCollectionModal(false)}
         onCollectionCreated={handleCollectionCreated}
       />
+      </div>
     </div>
   );
 }
